ace665cd59b2bfa92198efa3e0c1f9c3d412a4d2
emon3455 <emon.mhk69@gmail.com>
2023-01-27 23:48:29 +0600

    print 1-10 using while loop

---
+++ print1-100While.js
@@ -0,0 +1,8 @@
+// print 1-100 using whie loop,
+
+var number = 0;
+
+while (number <= 10){
+    console.log(number);
+    number++;
+}
---
+++ while-loop.js
@@ -0,0 +1,7 @@
+var count =0;
+var ar = ["Emon","Sumon","Yousuf","Habib"];
+
+while(count < ar.length){
+    console.log(ar[count]);
+    count++;
+}
