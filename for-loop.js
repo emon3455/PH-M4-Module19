013ce937367a0c2267b4c5745f2a5cc5590e8af6
emon3455 <emon.mhk69@gmail.com>
2023-01-28 10:00:07 +0600

    for loop done

---
+++ for-loop.js
@@ -0,0 +1,13 @@
+//Simple for loop:
+
+console.log("Simple for loop: ");
+for(var i=0; i<7 ; i++){
+    console.log(i);
+}
+
+// find the sum of 1-20 all numbers;
+var sum =0;
+for(var i=1; i <=20; i++){
+    sum = sum + i;
+}
+console.log("Sum of the all number 1-20 is : "+sum);
