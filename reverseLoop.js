095c5e8dc5e2eb8abd1e7908e8e3db054a45ccc5
emon3455 <emon.mhk69@gmail.com>
2023-01-28 11:16:42 +0600

    loop in reverse way

---
+++ reverseLoop.js
@@ -0,0 +1,12 @@
+// while loop in reverse way:
+var number = 10;
+while(number>=1){
+    process.stdout.write(number+ " ");
+    number--;
+}
+console.log("");
+
+// for loop in reverse way:
+for(i=10; i>=1; i--){
+    process.stdout.write(i+ " ");
+}
